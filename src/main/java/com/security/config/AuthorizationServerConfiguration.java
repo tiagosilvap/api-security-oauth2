04c879c4f41bc285378b4fd30566dd6b8f12d456
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-20 21:34:04 -0300

    Retornar token JWT no lugar do token opaco

--- src/main/java/com/security/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/security/config/AuthorizationServerConfiguration.java
@@ -1,70 +1,70 @@
-package com.security.config;
-
-import com.security.service.CustomUserDetailService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-
-import static com.security.config.ResourceServerConfiguration.RESOURCE_ID;
-
-@Configuration
-@RequiredArgsConstructor
-@EnableAuthorizationServer
-public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
-    
-    @Value("${app.client_id}")
-    private String clientId;
-    
-    @Value("${app.secret_id}")
-    private String secretId;
-    
-    
-    @Qualifier("authenticationManagerBean")
-    final AuthenticationManager authenticationManager;
-    
-    TokenStore tokenStore = new InMemoryTokenStore();
-    
-    private final CustomUserDetailService userDetailService;
-    
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(tokenStore)
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailService);
-    }
-    
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient(clientId)
-                .authorizedGrantTypes("password", "refresh_token")
-                .scopes("read", "write")
-                .secret(encoder().encode(secretId))
-                .resourceIds(RESOURCE_ID);
-    }
-    
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices tokenServices = new DefaultTokenServices();
-        tokenServices.setTokenStore(tokenStore);
-        return tokenServices;
-    }
-    
-    @Bean
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package com.security.config;
+//
+//import com.security.service.CustomUserDetailService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+//import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+//
+//import static com.security.config.ResourceServerConfiguration.RESOURCE_ID;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableAuthorizationServer
+//public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+//
+//    @Value("${app.client_id}")
+//    private String clientId;
+//
+//    @Value("${app.secret_id}")
+//    private String secretId;
+//
+//
+//    @Qualifier("authenticationManagerBean")
+//    final AuthenticationManager authenticationManager;
+//
+//    TokenStore tokenStore = new InMemoryTokenStore();
+//
+//    private final CustomUserDetailService userDetailService;
+//
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints.tokenStore(tokenStore)
+//                .authenticationManager(authenticationManager)
+//                .userDetailsService(userDetailService);
+//    }
+//
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.inMemory()
+//                .withClient(clientId)
+//                .authorizedGrantTypes("password", "refresh_token")
+//                .scopes("read", "write")
+//                .secret(encoder().encode(secretId))
+//                .resourceIds(RESOURCE_ID);
+//    }
+//
+//    @Bean
+//    @Primary
+//    public DefaultTokenServices tokenServices() {
+//        DefaultTokenServices tokenServices = new DefaultTokenServices();
+//        tokenServices.setTokenStore(tokenStore);
+//        return tokenServices;
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
---
+++ src/main/java/com/security/config/AuthorizationServerConfigurationJWT.java
@@ -0,0 +1,90 @@
+package com.security.config;
+
+import com.security.service.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import static com.security.config.ResourceServerConfiguration.RESOURCE_ID;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableAuthorizationServer
+public class AuthorizationServerConfigurationJWT extends AuthorizationServerConfigurerAdapter {
+    
+    @Value("${app.client_id}")
+    private String clientId;
+    
+    @Value("${app.secret_id}")
+    private String secretId;
+    
+    @Value("${app.signing_key_jwt}")
+    private String signingKey;
+    
+    private final AuthenticationManager authenticationManager;
+    
+    private final CustomUserDetailService userDetailService;
+    
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore())
+                .authenticationManager(authenticationManager)
+                .accessTokenConverter(accessTokenConverter())
+                .tokenEnhancer(accessTokenConverter());
+    }
+    
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient(clientId)
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("read", "write")
+                .secret(encoder().encode(secretId))
+                .resourceIds(RESOURCE_ID);
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(tokenStore());
+        tokenServices.setSupportRefreshToken(true);
+        return tokenServices;
+    }
+    
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+    
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        DefaultUserAuthenticationConverter duac = new DefaultUserAuthenticationConverter();
+        duac.setUserDetailsService(userDetailService);
+        
+        DefaultAccessTokenConverter datc = new DefaultAccessTokenConverter();
+        datc.setUserTokenConverter(duac);
+        
+        JwtAccessTokenConverter jatc = new JwtAccessTokenConverter();
+        jatc.setAccessTokenConverter(datc);
+        jatc.setSigningKey(signingKey);
+        return jatc;
+    }
+}
