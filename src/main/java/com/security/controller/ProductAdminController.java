b970c49736c9321f60fd9190bd2e03e92fe4bb02
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-22 00:57:45 -0300

    otimizando imports

--- src/main/java/com/security/controller/ProductAdminController.java
+++ src/main/java/com/security/controller/ProductAdminController.java
@@ -2,17 +2,11 @@ package com.security.controller;
 
 import com.security.components.IAuthenticationFacade;
 import com.security.model.Product;
-import com.security.model.Usuario;
 import com.security.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
