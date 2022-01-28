d12cffa43f4070176fee0f138bfe513db929ee11
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-28 01:28:23 -0300

    extraindo configuração de permissionamento do MethodSecurityConfig extends GlobalMethodSecurityConfiguration para a classe raiz ApiSecurityOauthApplication

--- src/main/java/com/security/ApiSecurityOauthApplication.java
+++ src/main/java/com/security/ApiSecurityOauthApplication.java
@@ -2,8 +2,13 @@ package com.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
 public class ApiSecurityOauthApplication {
     
     public static void main(String[] args) {
